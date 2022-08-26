20c4a030e6aee31bbe11f2b9ffc1303ab17c5506
Mehul Kumar <202151092@iiitvadodara.ac.in>
2022-08-26 13:05:13 +0530

    Day1 completed

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:codepur/pages/homePage.dart';
+import 'package:codepur/pages/login.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.purple),
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+      },
+    );
+  }
+}
---
+++ lib/pages/homePage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Catalog App"),
+      ),
+      body: Center(child: Text("Welcome to Catalod App")),
+      drawer: Drawer(),
+    );
+  }
+}
