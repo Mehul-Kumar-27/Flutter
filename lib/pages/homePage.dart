9291eb4d26f03eea86766be1121a2e93b1676d19
Mehul Kumar <202151092@iiitvadodara.ac.in>
2022-09-12 23:56:39 +0530

    VelocityX usage

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
+// ignore_for_file: public_member_api_docs, sort_constructors_first, prefer_const_constructors, prefer_const_literals_to_create_immutables
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
@@ -8,6 +8,7 @@ import 'package:codepur/models/catalog.dart';
 import 'package:codepur/widget/ItemWidgets.dart';
 import 'package:codepur/widget/drawer.dart';
 import 'dart:convert';
+import 'package:velocity_x/velocity_x.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -45,23 +46,108 @@ class _HomePageState extends State<HomePage> {
     final Email = routeData['Email'];
 
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text("Catalog App"),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child:
-              (CatalogueModel.items != null && CatalogueModel.items!.isNotEmpty)
-                  ? ListView.builder(
-                      itemCount: CatalogueModel.items!.length,
-                      itemBuilder: (context, index) {
-                        return ItemWidget(item: CatalogueModel.items![index]);
-                      })
-                  : Center(
-                      child: CircularProgressIndicator(),
-                    ),
-        ),
-        drawer: MyDrawer(Name.toString(), Email.toString()));
+      body: SafeArea(
+        child: Container(
+            color: Color.fromARGB(255, 246, 233, 248),
+            padding: Vx.m24,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CatalogueHeader(),
+                if (CatalogueModel.items != null &&
+                    CatalogueModel.items!.isNotEmpty)
+                  CatalogueList().py16().expand()
+                else
+                  CircularProgressIndicator().centered().expand(),
+              ],
+            )),
+      ),
+    );
+  }
+}
+
+class CatalogueHeader extends StatelessWidget {
+  const CatalogueHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        "Catalog App".text.xl4.bold.purple800.make(),
+        "Trending Products".text.xl.pink400.make(),
+      ],
+    );
+  }
+}
+
+class CatalogueList extends StatelessWidget {
+  // const CatalogList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: CatalogueModel.items!.length,
+      itemBuilder: (context, index) {
+        final catalog = CatalogueModel.items![index];
+        return CatalogItems(catalog: catalog);
+      },
+    );
+  }
+}
+
+class CatalogImage extends StatelessWidget {
+  final String image;
+  const CatalogImage({Key? key, required this.image}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(image)
+        .box
+        .color(Color.fromARGB(255, 232, 201, 236))
+        .rounded
+        .p12
+        .make()
+        .p12()
+        .w40(context);
+  }
+}
+
+class CatalogItems extends StatelessWidget {
+  final Item catalog;
+
+  const CatalogItems({Key? key, required this.catalog}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return VxBox(
+        child: Row(
+      children: [
+        CatalogImage(image: catalog.image),
+        Expanded(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            catalog.name.text.bold.make(),
+            catalog.desc.text.textStyle(context.captionStyle).make(),
+            10.heightBox,
+            ButtonBar(
+              alignment: MainAxisAlignment.spaceBetween,
+              children: [
+                "\$${catalog.price}".text.bold.xl.make(),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: "Buy".text.make(),
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.purple),
+                      shape: MaterialStateProperty.all(StadiumBorder())),
+                )
+              ],
+            )
+          ],
+        ))
+      ],
+    )).purple100.rounded.square(150).make().py12();
   }
 }
